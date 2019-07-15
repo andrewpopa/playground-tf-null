77c2d4e36521cb45566120fee7c1dad203c79bc8
Andrei Popa <andrei@hashicorp.com>
2019-07-15 14:30:55 +0200

    add random server

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "random_pet" "server" {}
 output "random_name" {
   value = "${random_pet.server.id}"
 }
+
+resource "random_id" "server" {
+  byte_length = 8
+}
+
+output "random_server" {
+  value = "${random_id.server.id}"
+}
